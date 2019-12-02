0deaef2b52d6fa0b3eaca7e94b7e82ab1557df24
kshashwat007 <kshashwat007@gmail.com>
2019-12-02 17:29:08 +0400

    Added replies
    Added Replies to specific questions and statement with volume, pitch and speech etc.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const button = document.querySelector(".talk");
 const content = document.querySelector(".content");
 
+const greetings = ["I'm good", "Doing good", "Go away"];
+const weather = ["Weather is good outside", "Its cloudy", "Shut up"];
+
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -13,8 +16,31 @@ recognition.onresult = function(event) {
   const current = event.resultIndex;
   const transcript = event.results[current][0].transcript;
   content.textContent = transcript;
+  readOutLoud(transcript);
 };
 
 button.addEventListener("click", () => {
   recognition.start();
 });
+
+function readOutLoud(message) {
+  const speech = new SpeechSynthesisUtterance();
+
+  speech.text = "I dont know what you said";
+
+  if (message.includes("How are you")) {
+    const finalText = greetings[Math.floor(Math.random() * greetings.length)];
+    speech.text = finalText;
+  }
+
+  if (message.includes("How is the weather")) {
+    const finalText = weather[Math.floor(Math.random() * greetings.length)];
+    speech.text = finalText;
+  }
+
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech);
+}
